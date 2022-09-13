4ac96a680bd9774fb3329a303d5148e83cffb682
zeus-kronosgios <113388612+zeus-kronosgios@users.noreply.github.com>
2022-09-13 13:44:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title ('My Parents New Healthy Diner')
+streamlit.title ('My Mom''s New Healthy Diner')
 
-streamlit.header ('Breakfast menu')
+streamlit.header ('Breakfast Favorites')
 streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text ('🥗 Kale, Spinach & Rocket smoothie')
 streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
