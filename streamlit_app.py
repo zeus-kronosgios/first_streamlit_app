2337b03d0d3b677cc571dbb804f3c52533d031b9
zeus-kronosgios <113388612+zeus-kronosgios@users.noreply.github.com>
2022-09-13 13:28:46 -0400

    Update streamlit_app.py
    Adding menuu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title ('my paprents new healthy diner')
+streamlit.title ('My Parents New Healthy Diner')
+
+streamlit.header ('Breakfast menu')
+streamlit.text ('Omega 3 & Blueberry Oatmeal')
+streamlit.text ('Kale, Spinach & Rocket smoothie')
+streamlit.text ('Hard-Boiled Free-Range Egg')
